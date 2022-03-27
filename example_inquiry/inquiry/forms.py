ce24d9d86d63963e56eb163e97c876cd21388796
Furi <furisep@gmail.com>
2022-03-27 19:51:09 +0900

    forms.pyにコメントを追加

--- example_inquiry/inquiry/forms.py
+++ example_inquiry/inquiry/forms.py
@@ -3,12 +3,14 @@ from django import forms
 from .models import Inquiry
 
 class InquiryForm1(forms.Form):
+    # CharFieldはフォームのテキスト型に対応
     name = forms.CharField(
         label='お名前',
-        max_length=40,
-        required=True
+        max_length=40, # max_lengthで長さを指定できる
+        required=True  # このフォームが入力必須かどうか
     )
 
+    # EmailFieldはフォームのemail型に対応
     email = forms.EmailField(
         label='メールアドレス',
         required=True
@@ -21,6 +23,6 @@ class InquiryForm1(forms.Form):
 
     contents = forms.CharField(
         label='内容',
-        widget=forms.Textarea
+        widget=forms.Textarea # フォームをtextarea要素に変更できる
     )
 
